adf9e30753c14a52667a6e4548f41ea5a99eafe3
jamal <ouardijamal87@gmail.com>
2023-09-06 09:54:35 +0100

    first

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M201_TP1_Layouts_bis"
+include(":app")
+ 
